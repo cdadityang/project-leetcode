933d6b8035abdbd669d54da5addfa6d91b5d7a62
Aditya Nagla <me@cdadityang.xyz>
2020-06-30 10:35:16 +0530

    Problem 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Project Leetcode
+
+**⚠️ Spoiler Alert: ⚠️**
+
+> This repo contains my solutions to [leetcode](https://leetcode.com) problems that I've tried to solve on my own + help from strangers, so that I can use them as reference when revising stuff up.
---
+++ problem-0001.rb
@@ -0,0 +1,19 @@
+# Two Sums
+
+## https://leetcode.com/problems/two-sum/
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  newArr = []
+  (0..nums.length - 1).each do |n|
+    ((n + 1)..nums.length - 1).each do |m|
+      newArr.push(n, m) if nums[n] + nums[m] == target && !newArr.include?(nums[n]) && !newArr.include?(nums[m])
+    end
+  end
+  newArr
+end
+
+p two_sum([2, 7, 11, 15], 9)
+# => [0, 1]
